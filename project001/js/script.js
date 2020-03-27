b1cef3598efa6ecb2a8d5d54f3873c0886c95df1
Aleksey Morozov <stranger140569@yandex.ru>
2020-03-27 19:50:00 +0300

    make slider

--- project001/js/script.js
+++ project001/js/script.js
@@ -8,6 +8,7 @@ window.addEventListener('DOMContentLoaded', function() {
         tabContent = document.querySelectorAll('.info-tabcontent');
 
     function hideTabContent(a) {
+
         for(let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
@@ -17,6 +18,7 @@ window.addEventListener('DOMContentLoaded', function() {
     hideTabContent(1);
 
     function showTabContent(b) {
+
         if(tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
@@ -35,11 +37,12 @@ window.addEventListener('DOMContentLoaded', function() {
                 }
             }
         }
+        
     });
 
     // timer
 
-    let deadline = '2021-03-27';
+    let deadline = '2021-03-31';
 
     function getTimeRemaining(endTime) {
         let t = Date.parse(endTime) - Date.parse(new Date()),
@@ -47,12 +50,12 @@ window.addEventListener('DOMContentLoaded', function() {
             minutes = Math.floor((t / 1000 / 60) % 60),
             hours = Math.floor((t / (1000 * 60 * 60)) % 60);
 
-        return {
-            'total': t,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds,
-        };
+            return {
+                'total': t,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds,
+            };
     }
 
     function setClock(id, endTime) {
@@ -66,6 +69,7 @@ window.addEventListener('DOMContentLoaded', function() {
             let t = getTimeRemaining(endTime);
 
             function addZero(num){
+
                 if(num <= 9) {
                     return '0' + num;
                 }
@@ -142,6 +146,7 @@ window.addEventListener('DOMContentLoaded', function() {
         request.send(json);
 
         request.addEventListener('readystatechange', function(){
+
             if(request.readyState < 4) {
                 statusMessage.innerHTML = message.loading;
             }
@@ -155,6 +160,64 @@ window.addEventListener('DOMContentLoaded', function() {
 
         for(let i = 0; i < input.length; i++) {
             input[i].value = '';
+        }
+    });
+
+    // slider
+
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+    
+    showSlides(slideIndex);
+    
+    function showSlides(n) {
+
+        if(n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if(n < 1) {
+            slideIndex = slides.length;
+        }
+
+        for(let i = 0; i < slides.length; i++) {
+            slides[i].style.display = 'none';
+        }
+
+        for(let i = 0; i < dots.length; i++) {
+            dots[i].classList.remove('dot-active');
+        }
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+
+        for(let i = 0; i < dots.length + 1; i++) {
+            if(event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+                currentSlide(i);
+            }
         }
     });
 });
